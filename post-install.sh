98f691e2a08d1f89a511f109d281beebf75ae3dc
Adam Popp <85586909+AdamPopp98@users.noreply.github.com>
2023-01-28 10:46:37 -0500

    added logo and switched to curl for downloads

--- post-install.sh
+++ post-install.sh
@@ -5,7 +5,7 @@ default_theme="Ascent"
 root_pswd=$1
 non_root_username=$2
 non_root_pswd=$3
-#install_script_repo="https://github.com/AdamPopp98/JoyOS_Post_Install_Script"
+installer_repo="https://raw.githubusercontent.com/AdamPopp98/JoyOS_Post_Install_Script/main"
 
 create_new_user()
 {
@@ -18,6 +18,7 @@ create_new_user()
 
 install_pacman_packages()
 {
+    pacman -Syu git
     while IFS=, read -r package_name;
     do
         sudo pacman -S --noconfirm $package_name;
@@ -53,46 +54,38 @@ install_aur_packages()
 create_new_user
 install_pacman_packages
 install_aur_packages
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.xinitrc /home/adam/.xinitrc
-rm ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.xinitrc
-
 
 #create directories for config files
 mkdir ~/.config
+curl "$installer_repo"/leaf-logo.png -o ~/.config/JoyOS-Logo.png
 
 mkdir ~/.config/alacritty
-touch ~/.config/alacritty/alacritty.yml
+curl "$installer_repo"/.config/alacritty/alacritty.yml -o ~/.config/alacritty/alacritty.yml
 
 mkdir ~/.config/joshuto
-touch ~/.config/joshuto/joshuto.toml
-touch ~/.config/joshuto/bookmarks.toml
-touch ~/.config/joshuto/mimetype.toml
-touch ~/.config/joshuto/theme.toml
-touch ~/.config/joshuto/keymap.toml
+curl "$installer_repo"/.config/joshuto/joshuto.toml -o ~/.config/joshuto/joshuto.toml
+curl "$installer_repo"/.config/joshuto/bookmarks.toml -o ~/.config/joshuto/bookmarks.toml
+curl "$installer_repo"/.config/joshuto/mimetype.toml -o ~/.config/joshuto/mimetype.toml
+curl "$installer_repo"/.config/joshuto/theme.toml -o ~/.config/joshuto/theme.toml
+curl "$installer_repo"/.config/joshuto/keymap.toml -o ~/.config/joshuto/keymap.toml
 
 mkdir ~/.config/leftwm
-touch ~/.config/leftwm/config.ron
-touch ~/.config/leftwm/themes.toml
+curl "$installer_repo"/.config/leftwm/themes.toml -o ~/.config/leftwm/themes.toml
+curl "$installer_repo"/.config/leftwm/config.ron -o ~/.config/leftwm/config.ron
 
 #Installs display manager, window manager and compositor
 leftwm-theme update
-leftwm-theme install "Ascent"
-leftwm-theme apply "Ascent"
+leftwm-theme install "$default_theme"
+leftwm-theme apply "$default_theme"
 leftwm-config -n # Generate new config
-
-#moves config files to proper directory
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/joshuto/joshuto.toml ~/.config/joshuto/joshuto.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/joshuto/bookmarks.toml ~/.config/joshuto/bookmarks.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/joshuto/mimetype.toml ~/.config/joshuto/mimetype.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/joshuto/keymap.toml ~/.config/joshuto/keymap.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/joshuto/theme.toml ~/.config/joshuto/theme.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/leftwm/themes.toml ~/.config/leftwm/themes.toml
-cp ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config/leftwm/config.ron ~/.config/leftwm/config.ron
 
 #removes config files after they have been copied over.
 rm -rf ~/JoyOS_Post_Install_Script/JoyOS_Post_Install_Script/.config
+clear
+echo "Clearing bash history to remove stored passwords"
 history -c
+echo "installation complete!\n\n"
+jp2a --colors ~/.config/JoyOS-Logo.png; figlet -w 87 "Welcome To JoyOS"
 #paru -S lightdm --no-confirm
 #sudo systemctl enable lightdm
 #Installs terminal utilities
